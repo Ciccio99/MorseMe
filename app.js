93680dbf76f26e1ac1d61aab389738b6be958667
Alberto Scicali <cicciobello99@gmail.com>
2015-01-02 19:58:34 -0600

    Changed morse storage, now works text -> morse and morse -> text

--- app.js
+++ app.js
@@ -3,74 +3,88 @@ var MorseNode = function(letter, morse){
 	this.morse = morse;
 };
 
-var morseArray = [];
+var morseDict = {
+	"A" : ".-",
+	"B" : "-...",
+	"C" : "-.-.",
+	"D" : "-.-",
+	"E" : ".",
+	"F" :  "..-.",
+	"G" : "--.",
+	"H" : "....",
+	"I" : "..",
+	"J" : ".---",
+	"K" : "-.-",
+	"L" : ".-..",
+	"M" :  "--",
+	"N" : "-.",
+	"O" : "---",
+	"P" : ".--.",
+	"Q" : "--.-",
+	"R" : ".-.",
+	"S" : "...",
+	"T" : "-",
+	"U" : "..-",
+	"V" : "...-",
+	"W" : ".--",
+	"X" : "-..-",
+	"Y" : "-.--",
+	"Z" : "--..",
+	"Ä" : ".-.-",
+	"Á" : ".--.-",
+	"Å" : ".--.-",
+	"É" : "..-..",
+	"Ñ" : "--.--",
+	"Ö" : "---.",
+	"Ü" : "..--",
+	"0" : "-----",
+	"1" : ".-----",
+	"2" : "..---",
+	"3" : "...--",
+	"4" : "....-",
+	"5" : ".....",
+	"6" : "-....",
+	"7" : "--...",
+	"8" : "---..",
+	"9" : "----.",
+	" " : "  "
+};
 
-morseArray.push(new MorseNode("A", ".-"));
-morseArray.push(new MorseNode("B", "-..."));
-morseArray.push(new MorseNode("C", "-.-."));
-morseArray.push(new MorseNode("D", "-.-"));
-morseArray.push(new MorseNode("E", "."));
-morseArray.push(new MorseNode("F", "..-."));
-morseArray.push(new MorseNode("G", "--."));
-morseArray.push(new MorseNode("H", "...."));
-morseArray.push(new MorseNode("I", ".."));
-morseArray.push(new MorseNode("J", ".---"));
-morseArray.push(new MorseNode("K", "-.-"));
-morseArray.push(new MorseNode("L", ".-.."));
-morseArray.push(new MorseNode("M", "--"));
-morseArray.push(new MorseNode("N", "-."));
-morseArray.push(new MorseNode("O", "---"));
-morseArray.push(new MorseNode("P", ".--."));
-morseArray.push(new MorseNode("Q", "--.-"));
-morseArray.push(new MorseNode("R", ".-."));
-morseArray.push(new MorseNode("S", "..."));
-morseArray.push(new MorseNode("T", "-"));
-morseArray.push(new MorseNode("U", "..-"));
-morseArray.push(new MorseNode("V", "...-"));
-morseArray.push(new MorseNode("W", ".--"));
-morseArray.push(new MorseNode("X", "-..-"));
-morseArray.push(new MorseNode("Y", "-.--"));
-morseArray.push(new MorseNode("Z", "--.."));
-morseArray.push(new MorseNode("Ä", ".-.-"));
-morseArray.push(new MorseNode("Á", ".--.-"));
-morseArray.push(new MorseNode("Å", ".--.-"));
-morseArray.push(new MorseNode("Ch", "----"));
-morseArray.push(new MorseNode("É", "..-.."));
-morseArray.push(new MorseNode("Ñ", "--.--"));
-morseArray.push(new MorseNode("Ö", "---."));
-morseArray.push(new MorseNode("Ü", "..--"));
-morseArray.push(new MorseNode("0","-----"));
-morseArray.push(new MorseNode("1",".-----"));
-morseArray.push(new MorseNode("2","..---"));
-morseArray.push(new MorseNode("3","...--"));
-morseArray.push(new MorseNode("4","....-"));
-morseArray.push(new MorseNode("5","....."));
-morseArray.push(new MorseNode("6","-...."));
-morseArray.push(new MorseNode("7","--..."));
-morseArray.push(new MorseNode("8","---.."));
-morseArray.push(new MorseNode("9","----."));
-
-var Translate = function(text){
+var TranslateText = function(text){
 	var morseCode = '';
 	for(var i = 0; i < text.length; i++){
 		var letter = text[i];
-		for(node in morseArray){
-			morseLetter = morseArray[node].letter;
-			if( morseLetter === letter.toUpperCase()){
-				morseCode += morseArray[node].morse + " ";
-				break;
-			}
-		}
+		morseCode += morseDict[letter.toUpperCase()] + " ";
 	}
 	return morseCode;
 };
 
+var TranslateMorse = function(text){
+	var morseArray = text.split(" ");
+	var textCode = '';
+	for(i in morseArray){
+		dictTraverse:
+		for(key in morseDict){
+			if(morseArray[i] === morseDict[key]){
+				textCode += key;
+				break dictTraverse;
+			}
+		}
+	}
+	return textCode;
+};
+
 var main = function(){
 	$('#Text_Area').keyup(function(){
-		
 		var content = $(this).val();
-		var translated = Translate(content);
+		var translated = TranslateText(content);
 		$('#Morse_Area').val(translated);
+	});
+
+	$('#Morse_Area').keyup(function(){
+		var content = $(this).val();
+		var translated = TranslateMorse(content);
+		$('#Text_Area').val(translated);
 	});
 }
 
